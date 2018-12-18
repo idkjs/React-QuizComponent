79ac867786371ffd63b8a7e285f1e89e72c608d5
Enso <aarmand.inbox@gmail.com>
2018-12-18 15:25:14 +0100

    Completed the module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import './App.css'
-
+import React, { Component } from 'react';
+import './App.css';
+import Quiz from './Quiz.js';
 class App extends Component {
-  render() {
-    return (
-      <div />
-    )
-  }
+	render() {
+		return (
+			<div>
+				<Quiz />
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
